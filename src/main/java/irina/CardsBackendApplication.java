8effe5bd84613ed28daa36ead569cce07e72dfb8
Irina Pankratova <pairvale@gmail.com>
2018-07-10 17:21:14 +0300

    Exploring spring boot - first draft.

--- src/main/java/irina/CardsBackendApplication.java
+++ src/main/java/irina/CardsBackendApplication.java
@@ -1,12 +1,12 @@
-package irina;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class CardsBackendApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(CardsBackendApplication.class, args);
-    }
-}
+//package irina;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class CardsBackendApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(CardsBackendApplication.class, args);
+//    }
+//}
