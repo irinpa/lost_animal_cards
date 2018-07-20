e188469d81f91d2375c218421448fe9fa9c7726a
Irina Pankratova <pairvale@gmail.com>
2018-07-20 16:24:32 +0300

    Add "getCardById" method to Card controller

--- src/main/java/irina/controllers/CardController.java
+++ src/main/java/irina/controllers/CardController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
@@ -18,6 +19,12 @@ public class CardController {
         List<Card> cards = cardRepository.findAll();
 
         return cards;
+    }
+    @GetMapping(value = "/cards/{cardId}", produces = "application/json")
+    public Card getCardById( @PathVariable("cardId") Long cardId){
+        Optional<Card> card = cardRepository.findById(cardId);
+
+        return card.get();
     }
 
     @PostMapping("/cards")
